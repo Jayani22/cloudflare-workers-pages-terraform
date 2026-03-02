9f058d723df389702226ae37443caad66ab7110c
Jayani22 <mutcherlajayani@gmail.com>
2026-03-02 21:44:46 +0530

    Cloudflare Workers and Pages setup

--- backend/worker.js
+++ backend/worker.js
@@ -6,6 +6,11 @@ async function handleRequest(request) {
   return new Response(JSON.stringify({
     message: "Hello from Cloudflare Worker 🚀"
   }), {
-    headers: { "Content-Type": "application/json" }
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
   })
 }
